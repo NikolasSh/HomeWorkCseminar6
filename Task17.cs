b98d0fb25b6aef63dee7476ab764a61e1aa94bad
Nikolai <107433192+NikolasSh@users.noreply.github.com>
2023-02-14 21:48:52 +0300

    Adds home work files

---
+++ Task17.cs
@@ -0,0 +1,65 @@
+﻿/*
+ Пользователь вводит с клавиатуры M чисел. 
+ Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result ))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Введено неверное число! Повторите ввод!");
+        }
+    }
+
+    return result;
+}
+
+int GetNumberNumbers(int num)
+{
+    int sumPositiveNumbers = 0;
+    int number = 0;
+    int[] array = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        number = GetNumber("Введите целое число: ");
+        array[i] = number;
+        if (number > 0)
+        {
+            sumPositiveNumbers = sumPositiveNumbers + 1;
+        }
+                
+    }
+
+    PrintArray(array);
+      
+
+    return sumPositiveNumbers;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int number = GetNumber("Сколько чисел вы хотите ввести?");
+int result = GetNumberNumbers(number);
+Console.WriteLine();
+Console.WriteLine($"Из {number} введённых чисел {result} положительных!");
+
+
---
+++ Task18.cs
@@ -0,0 +1,50 @@
+﻿/*
+Напишите программу, которая найдёт точку пересечения 
+двух прямых, заданных уравнениями y = k1 * x + b1
+                                  y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+double GetNumber(string message)
+{
+    double result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (double.TryParse(Console.ReadLine(), out result ))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Введено неверное число! Повторите ввод!");
+        }
+    }
+
+    return result;
+}
+
+void IntersectionPoint()
+{
+    double b1 = GetNumber("Введите значение b1: ");
+    double b2 = GetNumber("Введите значение b2: ");
+    double k1 = GetNumber("Введите значение k1: ");
+    double k2 = GetNumber("Введите значение k2: ");
+    double x = -(b1 - b2) / (k1 - k2);
+    double y = k1 * x + b1;
+    x = Math.Round(x, 3);
+    y = Math.Round(y, 3);
+    Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2}");
+    Console.WriteLine($"Пересечение в точке Х = {x} и Y = {y}");
+
+    
+
+}
+
+IntersectionPoint();
+ 
+
+ 
+
